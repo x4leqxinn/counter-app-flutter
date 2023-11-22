7cf54ce755ab4a149ceb3350b64055d6207ae492
Jorge Quintui <jorge.quintui.vergara@gmail.com>
2023-11-22 00:40:12 -0300

    add(lib): first component

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:my_app/screens/home_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -10,9 +11,6 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-      home: Center(
-        child: Text('Hola Mundo'),
-      ),
-    );
+        debugShowCheckedModeBanner: false, home: HomeScreen());
   }
 }
