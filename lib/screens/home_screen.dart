26aca71e57fb0dc44ca14067fa593377ec7a1a64
Jorge Quintui <jorge.quintui.vergara@gmail.com>
2023-11-22 01:51:40 -0300

    add(lib): stateless widget problem

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -2,30 +2,36 @@ import 'package:flutter/material.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({Key? key}) : super(key: key);
-  static const customFont = TextStyle(fontSize: 30);
+  // variable, propiedad
 
   @override
   Widget build(BuildContext context) {
+    const customFont = TextStyle(fontSize: 30);
+    int counter = 10;
+
     return Scaffold(
         appBar: AppBar(
           title: const Text('Home Screen'),
           elevation: 0,
         ),
         backgroundColor: Colors.pink,
-        body: const Center(
+        body: Center(
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             crossAxisAlignment: CrossAxisAlignment.center,
             children: <Widget>[
-              Text('Clicks counter', style: customFont),
-              Text('10', style: customFont),
+              const Text('Clicks counter', style: customFont),
+              Text('$counter', style: customFont),
             ],
           ),
         ),
         floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
         floatingActionButton: FloatingActionButton(
           child: const Icon(Icons.add),
-          onPressed: () {},
+          onPressed: () {
+            counter++;
+            print('value $counter');
+          },
         ));
   }
 }
